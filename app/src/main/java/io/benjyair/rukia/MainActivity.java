96e87ecfd776bdc6269e37581ece1593a69f8881
Benjy <280995163@qq.com>
2018-10-25 00:33:17 +0800

    Add function of lunar

--- app/src/main/java/io/benjyair/rukia/MainActivity.java
+++ app/src/main/java/io/benjyair/rukia/MainActivity.java
@@ -20,13 +20,17 @@ public class MainActivity extends AppCompatActivity implements TimeListener {
 
     private static final int WHAT_TIME = 0x01;
     private static final int WHAT_WEATHER = 0x02;
+    private static final int WHAT_LUNAR = 0x03;
 
     private static final SimpleDateFormat SDF_TIME = new SimpleDateFormat("HH:mm:ss", Locale.CHINA);
     private static final SimpleDateFormat SDF_DATE =
             new SimpleDateFormat("yyyy年MM月dd日  E  a", Locale.CHINA);
 
-    private FallTextView ftv_time;
     private FallTextView ftv_date;
+    private FallTextView ftv_lunar;
+    private FallTextView ftv_suit;
+    private FallTextView ftv_taboo;
+    private FallTextView ftv_time;
     private FallTextView ftv_weather;
     private FallTextView ftv_notice;
     private TimeHandler timeHandler = new TimeHandler();
@@ -43,14 +47,18 @@ public class MainActivity extends AppCompatActivity implements TimeListener {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        ftv_time = findViewById(R.id.ftv_time);
         ftv_date = findViewById(R.id.ftv_date);
+        ftv_lunar = findViewById(R.id.ftv_lunar);
+        ftv_suit = findViewById(R.id.ftv_suit);
+        ftv_taboo = findViewById(R.id.ftv_taboo);
+        ftv_time = findViewById(R.id.ftv_time);
         ftv_weather = findViewById(R.id.ftv_weather);
         ftv_notice = findViewById(R.id.ftv_notice);
 
         timeHandler.setTimerListener(this);
         timeHandler.addMessage(WHAT_TIME, 1000L);
         timeHandler.addMessage(WHAT_WEATHER, 60 * 60 * 1000L);
+        timeHandler.addMessage(WHAT_LUNAR, 60 * 60 * 1000L);
     }
 
     @Override
@@ -81,6 +89,9 @@ public class MainActivity extends AppCompatActivity implements TimeListener {
                 break;
             case WHAT_WEATHER:
                 getWeather();
+                break;
+            case WHAT_LUNAR:
+                getLunar();
                 break;
         }
         return true;
@@ -122,6 +133,43 @@ public class MainActivity extends AppCompatActivity implements TimeListener {
                                     today.getAqi(),
                                     today.getNotice());
                             ftv_notice.animateText(noticeStr);
+                        }
+                    });
+                } catch (Exception e) {
+                    // Nothing
+                }
+            }
+        }).start();
+    }
+
+    private void getLunar() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String apiUrl = "https://www.sojson.com/open/api/lunar/json.shtml";
+                    String result = httpUtil.get(apiUrl);
+                    if (TextUtils.isEmpty(result)) {
+                        return;
+                    }
+                    final Lunar lunar = gson.fromJson(result, Lunar.class);
+                    if (lunar == null || lunar.getStatus() != 200) {
+                        return;
+                    }
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Lunar.DataBean data = lunar.getData();
+                            String lunarStr = String.format(
+                                    "%s年%s月%s [%s年]",
+                                    data.getHyear(),
+                                    data.getCnmonth(),
+                                    data.getCnday(),
+                                    data.getAnimal()
+                            );
+                            ftv_lunar.animateText(lunarStr);
+                            ftv_suit.animateText("益：" + data.getSuit());
+                            ftv_taboo.animateText("忌：" + data.getTaboo());
                         }
                     });
                 } catch (Exception e) {
